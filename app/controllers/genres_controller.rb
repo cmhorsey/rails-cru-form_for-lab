9d3e1526c78dab2f2562a4c8c7be102c10fe9284
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-12 14:16:43 -0400

    Create genre edit form

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -14,6 +14,17 @@ class GenresController < ApplicationController
     redirect_to genre_path(@genre)
   end
 
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
+  end
+private
+
   def genre_params(*args)
     params.require(:genre).permit(*args)
   end
