00575565c5c2d95676cc600dd1e12b2ad0f5b489
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-12 14:10:16 -0400

    Create genre form

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,20 @@
 class GenresController < ApplicationController
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+		@genre = Genre.new
+	end
+
+  def create
+    @genre = Genre.new(genre_params(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  def genre_params(*args)
+    params.require(:genre).permit(*args)
+  end
 end
